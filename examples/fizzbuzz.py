c3165cd291b38952630b6dafb4aee5fbf1e801a2
bruzz-bruzz <frkhor@gmail.com>
2026-08-30 16:51:49 +0800

    examples: Refactor FizzBuzz to use if/elif/else and and

--- examples/fizzbuzz.py
+++ examples/fizzbuzz.py
@@ -1,15 +1,13 @@
 # FizzBuzz example - prints numbers 1 to 20, but for multiples of 3 prints "Fizz",
 # for multiples of 5 prints "Buzz", and for multiples of both 3 and 5 prints "FizzBuzz".
-# Demonstrates: modulo (%), and nested if/else control flow.
+# Demonstrates: modulo (%), if/elif/else chains, and for-loops.
 
 for i in range(1, 21):
-    if i % 3 == 0:
-        if i % 5 == 0:
-            print("FizzBuzz")
-        else:
-            print("Fizz")
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
     else:
-        if i % 5 == 0:
-            print("Buzz")
-        else:
-            print(i)
+        print(i)
